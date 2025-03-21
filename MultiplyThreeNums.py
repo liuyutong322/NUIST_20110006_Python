3ef8b2a8875d435e7d57834ee05346013df24c4f
liuyutong322 <3269076158@qq.com>
2025-03-21 15:48:05 +0000

    Product of three numbers

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,5 @@
+num1 = float(input("Please input the first number: "))
+num2 = float(input("Please input the secend number: "))
+num3 = float(input("Please input the thired number: "))
+product = num1 * num2 * num3
+print(f"The product of three numbers is {product}")
